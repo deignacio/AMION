16dde01fcb7bc46c7d8c643ec23e29e049d7a9cb
David Ignacio <deignacio@gmail.com>
2010-03-27 09:07:49 -0400

    remove trailing whitespace, convert tabs to spaces, use unix newlines

--- src/com/delauneconsulting/AMION/AMIONPerson.java
+++ src/com/delauneconsulting/AMION/AMIONPerson.java
@@ -2,20 +2,20 @@ package com.delauneconsulting.AMION;
 
 public class AMIONPerson {
 
-	public String firstName = "";
-	public String lastName = "";
-	public String currentJob = "";
-	public String comment = "";
-	public boolean existsInContacts = false;
-	public String phoneNumber = "";
-	
-	public AMIONPerson() {
-	}
-	
-	public String toString() {
-		if (firstName.length() > 0)
-			return lastName + ", " + firstName;
-		else
-			return lastName;
-	}
+    public String firstName = "";
+    public String lastName = "";
+    public String currentJob = "";
+    public String comment = "";
+    public boolean existsInContacts = false;
+    public String phoneNumber = "";
+
+    public AMIONPerson() {
+    }
+
+    public String toString() {
+        if (firstName.length() > 0)
+            return lastName + ", " + firstName;
+        else
+            return lastName;
+    }
 }
--- src/com/delauneconsulting/AMION/Helper.java
+++ src/com/delauneconsulting/AMION/Helper.java
@@ -8,43 +8,40 @@ import android.util.Log;
 
 public class Helper {
 
-	public static String getHttpResponseAsString(String url) {
-		
-	  String response = "";
-	  StringBuffer sb = new StringBuffer(); 
-	  InputStream is = null;
-      
-	  try {
-		  URL connectURL = new URL(url);
-	 	  HttpURLConnection conn = (HttpURLConnection)connectURL.openConnection(); 
-	 	  conn.setDoInput(true); 
-	 	  conn.setDoOutput(true); 
-	 	  conn.setUseCaches(false); 
-	 	  conn.setRequestMethod("GET"); 
-	 	  conn.connect();
-	 	  conn.getOutputStream().flush();
- 	  
-          is = conn.getInputStream(); 
-          int ch; 
-          while( ( ch = is.read() ) != -1 ) { 
-              sb.append( (char)ch );
-          }
-          
-          response = sb.toString(); 
-      }
-      catch(Exception e)
-      {
-         Log.e("ERR", "biffed it getting HTTPResponse");
-      }
-      finally 
-      {
-          try {
-          if (is != null)
-              is.close();
-          } catch (Exception e) {}
-      }
+    public static String getHttpResponseAsString(String url) {
 
-      return response;
-	}
-	
+        String response = "";
+        StringBuffer sb = new StringBuffer();
+        InputStream is = null;
+
+        try {
+            URL connectURL = new URL(url);
+            HttpURLConnection conn = (HttpURLConnection) connectURL.openConnection();
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+            conn.setUseCaches(false);
+            conn.setRequestMethod("GET");
+            conn.connect();
+            conn.getOutputStream().flush();
+
+            is = conn.getInputStream();
+            int ch;
+            while ((ch = is.read()) != -1) {
+                sb.append((char) ch);
+            }
+
+            response = sb.toString();
+        } catch (Exception e) {
+            Log.e("ERR", "biffed it getting HTTPResponse");
+        } finally {
+            try {
+                if (is != null)
+                    is.close();
+            } catch (Exception e) {
+            }
+        }
+
+        return response;
+    }
+
 }
