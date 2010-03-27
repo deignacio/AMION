ca4d57d3388a38e59c37e45f6b62744c3ed14de9
David Ignacio <deignacio@gmail.com>
2010-03-27 09:15:18 -0400

    parameterize AMIONPersonComparator, implement Comparator<AMIONPerson>

--- src/com/delauneconsulting/AMION/AMIONPersonComparator.java
+++ src/com/delauneconsulting/AMION/AMIONPersonComparator.java
@@ -2,13 +2,13 @@ package com.delauneconsulting.AMION;
 
 import java.util.Comparator;
 
-public class AMIONPersonComparator implements Comparator {
-    public int compare(Object p1, Object p2) {
+public class AMIONPersonComparator implements Comparator<AMIONPerson> {
+    public int compare(AMIONPerson p1, AMIONPerson p2) {
 
         // parameter are of type Object, so we have to downcast it to Employee
         // objects
-        String p1Name = ((AMIONPerson) p1).toString();
-        String p2Name = ((AMIONPerson) p2).toString();
+        String p1Name = p1.toString();
+        String p2Name = p2.toString();
 
         // uses compareTo method of String class to compare names of the
         // employee
